3c34c23b22f92d64c58a2a919f84c839d5b09f80
doseOfMazen <rexnowa@outlook.com>
2020-05-20 01:50:21 -0500

    Prettifying the code 💅

--- server.js
+++ server.js
@@ -4,26 +4,26 @@ let querystring = require("querystring");
 let app = express();
 let redirect_uri = process.env.REDIRECT_URI || "http://localhost:8888/callback";
 
-app.get("/login", function(req, res) {
+app.get("/login", function (req, res) {
   res.redirect(
     "https://accounts.spotify.com/authorize?" +
       querystring.stringify({
         response_type: "code",
         client_id: process.env.SPOTIFY_CLIENT_ID,
         scope: "user-read-private user-read-email playlist-read-private",
-        redirect_uri
+        redirect_uri,
       })
   );
 });
 
-app.get("/callback", function(req, res) {
+app.get("/callback", function (req, res) {
   let code = req.query.code || null;
   let authOptions = {
     url: "https://accounts.spotify.com/api/token",
     form: {
       code: code,
       redirect_uri,
-      grant_type: "authorization_code"
+      grant_type: "authorization_code",
     },
     headers: {
       Authorization:
@@ -32,11 +32,11 @@ app.get("/callback", function(req, res) {
           process.env.SPOTIFY_CLIENT_ID +
             ":" +
             process.env.SPOTIFY_CLIENT_SECRET
-        ).toString("base64")
+        ).toString("base64"),
     },
-    json: true
+    json: true,
   };
-  request.post(authOptions, function(error, response, body) {
+  request.post(authOptions, function (error, response, body) {
     var access_token = body.access_token;
     let uri = process.env.FRONTEND_URI || "http://localhost:3000";
     res.redirect(uri + "?access_token=" + access_token);
